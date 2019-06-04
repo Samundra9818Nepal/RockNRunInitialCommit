c9370c094ce10df3fdf62ea7e20dcbc4118a7658
Mr. Beast <1011025@student.sae.edu.au>
2019-06-04 14:16:49 +1000

    Initial Commit
    Initial commit-Player movement+Jump

---
+++ Assets/Scripts/CameraLookAT.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAT : MonoBehaviour
+{
+   public float FollowSpeed = 2f;
+    public Transform Target; // player to follow
+
+
+
+    // camera follows the player.
+    private void Update()
+    {
+        Vector3 newPosition = Target.position;
+        newPosition.z = -10;
+        transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMOvement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMOvement : MonoBehaviour
+{
+
+    public GameObject Concert; // Refrencing the object that player need to reach.
+    public float Speed = 10f;
+    public float JumpForce = 10f;
+
+
+
+
+    // moving player towards the concert gameobject position.
+    public void Update()
+    {
+
+        transform.position = Vector2.MoveTowards(transform.position, Concert.transform.position, Speed * Time.deltaTime);
+
+        Jump();
+    }
+
+    public void Jump ()
+
+    {
+
+
+
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+
+            this.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * JumpForce);
+        }
+    }
+
+
+}
