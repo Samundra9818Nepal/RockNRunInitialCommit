7acaa5f309a9e10fa4ac0411cf8869474187f2c3
Samundra <1011025@student.sae.edu.au>
2019-06-05 07:26:49 +1000

    Jump
    Fixed the double air jump. Now the player can jump once and can jump again after it hit's the ground.

--- Assets/Scripts/PlayerMOvement.cs
+++ Assets/Scripts/PlayerMOvement.cs
@@ -9,8 +9,16 @@ public class PlayerMOvement : MonoBehaviour
     public float Speed = 10f;
     public float JumpForce = 10f;
 
+    bool IsGrounded; // For Checking if the player Grounded.
 
 
+
+
+    public void Start()
+    {
+        // Making sure that player is grounded.
+        IsGrounded = true;
+    }
 
     // moving player towards the concert gameobject position.
     public void Update()
@@ -21,17 +29,26 @@ public class PlayerMOvement : MonoBehaviour
         Jump();
     }
 
+    // making player jump using J Key. 
     public void Jump ()
-
     {
-
-
-
-        if(Input.GetKeyDown(KeyCode.J))
+        if(Input.GetKeyDown(KeyCode.J)&& IsGrounded)
         {
-
             this.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * JumpForce);
+            IsGrounded = false;
         }
+    }
+
+    // Cheking that player has collide with the ground or not.
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if(collision.gameObject.tag == "Ground"&& IsGrounded == false)
+        {
+            IsGrounded = true;
+            Debug.Log("IsGrounded");
+        }
+
     }
 
 
