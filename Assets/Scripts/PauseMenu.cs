f25026ff918674840d48264a0aba420b458104b9
Samundra <1011025@student.sae.edu.au>
2019-06-05 11:42:55 +1000

    PauseMenu
    Just created a pause menu as it didn't have any UI but if you hit P it will pause the game and if you hit R it will Resume the game.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public void Update()
+    {
+
+        Pause_Menu(); // calling the Pause function
+
+    }
+
+
+
+    // Pause the game
+    public void Pause_Menu()
+
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+            {
+
+            Time.timeScale = 0f;
+
+        }
+
+
+
+        // Resume the Game
+        if(Input.GetKeyDown(KeyCode.R))
+            {
+
+            Time.timeScale = 1f;
+
+        }
+
+    }
+
+
+}
