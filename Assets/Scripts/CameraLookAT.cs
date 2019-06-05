6e4bcc10f50cbd6f57702da2b24ccb3cb977d922
Sam <1011025@student.sae.edu.au>
2019-06-05 11:32:01 +1000

    Scene Management/Camera
    Fixed the camera angle.
    Created a two scene and a script that fade to another after player reached the concert

--- Assets/Scripts/CameraLookAT.cs
+++ Assets/Scripts/CameraLookAT.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class CameraLookAT : MonoBehaviour
 {
-   public float FollowSpeed = 2f;
+   public float SmoothSpeed = 2f;
     public Transform Target; // player to follow
+    public Vector3 OffSet;
 
 
 
     // camera follows the player.
     private void Update()
     {
-        Vector3 newPosition = Target.position;
-        newPosition.z = -10;
-        transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
+        Vector3 newPosition = Target.position + OffSet;
+        Vector3 SmoothPosition = Vector3.Lerp(transform.position, newPosition, SmoothSpeed * Time.deltaTime);
+        transform.position = SmoothPosition;
+       // newPosition.z = -10;
+        //transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+
+
+    public string LevelName = "Scene 2";
+
+
+
+    //Load New Scene
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+
+
+            SceneManager.LoadScene(LevelName);
+
+        }
+
+    }
+
+}
