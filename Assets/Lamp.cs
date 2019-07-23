a4ccfcac12ccd3152a434bc2332901cba11a5184
Kartik <1013041@student.sae.edu.au>
2019-07-23 13:13:41 +1000

    Lamp Light implementation (WIP)
    Lamp Light implementation (WIP)

---
+++ Assets/Lamp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour
+{
+    public Color colorInitial;
+    public Color color2;
+    public SpriteRenderer spriteRef;
+    void Start()
+    {
+        spriteRef = GetComponent<SpriteRenderer>();
+        colorInitial = spriteRef.color;
+        color2 = new Color(0, 170, 255);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spriteRef.color = Color.Lerp(colorInitial, color2, Mathf.PingPong(Time.time,1));
+    }
+}
