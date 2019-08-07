bf95cfc3917a18c9a5eb6289b6b52f7ae2b5f044
Kartik <1013041@student.sae.edu.au>
2019-08-07 13:34:16 +1000

    UI EDM 2

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score;
+    public int noteCount;
+    public int multiplier;
+    public string totalNoteCount;
+
+    Text scoreT;
+    Text noteCountT;
+    Text multiplierT;
+    void Awake ()
+    {
+        score = 0;
+        noteCount = 0;
+        multiplier = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreT.text = "Score: " + score;
+        noteCountT.text = noteCount.ToString();
+        multiplierT.text = multiplier.ToString();
+    }
+}
