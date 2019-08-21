cb5ebbd604235e1fa1174f6e48e08e92d4936e27
Samundra <1011025@student.sae.edu.au>
2019-08-21 19:00:13 +1000

    Changed the Rock level 2
    Everything is working fine...

--- Assets/Scripts/Winningleveltoload.cs
+++ Assets/Scripts/Winningleveltoload.cs
@@ -16,6 +16,7 @@ public class Winningleveltoload : MonoBehaviour
         
     if(collision.gameObject.tag == "Player")
         {
+
           ScoreBoi.GetComponent<Scores>().GetHighScore();
 
             PlayerPrefs.SetInt("levelReached", LevelToUnlock);
